69339a26317f391972153522f30fbc5f843bd607
100nka <krystiankloniecki@gmail.com>
2015-04-14 13:39:42 +0200

    Revert "Revert "add login user informations""
    This reverts commit 68825e8242459dc9c58dc8397a45da08fbd2bc96.

--- metrostylegui/LoginInput.Designer.cs
+++ metrostylegui/LoginInput.Designer.cs
@@ -32,11 +32,17 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.bLog = new MetroFramework.Controls.MetroButton();
-            this.metroProgressSpinner1 = new MetroFramework.Controls.MetroProgressSpinner();
             this.lLogin = new MetroFramework.Controls.MetroLabel();
             this.lPwd = new MetroFramework.Controls.MetroLabel();
             this.tPwd = new MetroFramework.Controls.MetroTextBox();
             this.tLogin = new MetroFramework.Controls.MetroTextBox();
+            this.metroProgressSpinner1 = new MetroFramework.Controls.MetroProgressSpinner();
+            this.ILastLogin = new MetroFramework.Controls.MetroLabel();
+            this.ILastName = new MetroFramework.Controls.MetroLabel();
+            this.tlastname = new MetroFramework.Controls.MetroTextBox();
+            this.tlastlogin = new MetroFramework.Controls.MetroTextBox();
+            this.tfirstname = new MetroFramework.Controls.MetroTextBox();
+            this.IFirstName = new MetroFramework.Controls.MetroLabel();
             this.panelbody.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -50,33 +56,46 @@
             this.panelbody.Location = new System.Drawing.Point(0, 0);
             this.panelbody.Margin = new System.Windows.Forms.Padding(0);
             this.panelbody.Name = "panelbody";
-            this.panelbody.Size = new System.Drawing.Size(600, 110);
+            this.panelbody.Size = new System.Drawing.Size(600, 242);
             this.panelbody.TabIndex = 8;
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel1.ColumnCount = 4;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 210F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 3);
-            this.tableLayoutPanel1.Controls.Add(this.metroProgressSpinner1, 3, 1);
-            this.tableLayoutPanel1.Controls.Add(this.lLogin, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.lPwd, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.tPwd, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.tLogin, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.lLogin, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lPwd, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.tPwd, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.tLogin, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.metroProgressSpinner1, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.ILastLogin, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ILastName, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tlastname, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tlastlogin, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tfirstname, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.IFirstName, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.RowCount = 8;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(600, 110);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(600, 242);
             this.tableLayoutPanel1.TabIndex = 12;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // tableLayoutPanel2
             // 
@@ -89,7 +108,7 @@
             this.tableLayoutPanel2.Controls.Add(this.bLog, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(150, 80);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(150, 180);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
@@ -113,26 +132,13 @@
             this.bLog.UseSelectable = true;
             this.bLog.Click += new System.EventHandler(this.bLogin_Click);
             // 
-            // metroProgressSpinner1
-            // 
-            this.metroProgressSpinner1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.metroProgressSpinner1.Location = new System.Drawing.Point(498, 23);
-            this.metroProgressSpinner1.Maximum = 100;
-            this.metroProgressSpinner1.Name = "metroProgressSpinner1";
-            this.tableLayoutPanel1.SetRowSpan(this.metroProgressSpinner1, 2);
-            this.metroProgressSpinner1.Size = new System.Drawing.Size(53, 54);
-            this.metroProgressSpinner1.TabIndex = 14;
-            this.metroProgressSpinner1.UseSelectable = true;
-            this.metroProgressSpinner1.UseStyleColors = true;
-            this.metroProgressSpinner1.Visible = false;
-            // 
             // lLogin
             // 
             this.lLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.lLogin.Location = new System.Drawing.Point(150, 24);
+            this.lLogin.Location = new System.Drawing.Point(150, 124);
             this.lLogin.Margin = new System.Windows.Forms.Padding(0);
             this.lLogin.Name = "lLogin";
-            this.lLogin.Size = new System.Drawing.Size(100, 22);
+            this.lLogin.Size = new System.Drawing.Size(90, 22);
             this.lLogin.TabIndex = 6;
             this.lLogin.Text = "Login:";
             this.lLogin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -141,10 +147,10 @@
             // lPwd
             // 
             this.lPwd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.lPwd.Location = new System.Drawing.Point(150, 54);
+            this.lPwd.Location = new System.Drawing.Point(150, 154);
             this.lPwd.Margin = new System.Windows.Forms.Padding(0);
             this.lPwd.Name = "lPwd";
-            this.lPwd.Size = new System.Drawing.Size(100, 22);
+            this.lPwd.Size = new System.Drawing.Size(90, 22);
             this.lPwd.TabIndex = 7;
             this.lPwd.Text = "Password:";
             this.lPwd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -154,14 +160,14 @@
             // 
             this.tPwd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.tPwd.Lines = new string[0];
-            this.tPwd.Location = new System.Drawing.Point(250, 54);
+            this.tPwd.Location = new System.Drawing.Point(240, 154);
             this.tPwd.Margin = new System.Windows.Forms.Padding(0);
             this.tPwd.MaxLength = 32767;
             this.tPwd.Name = "tPwd";
-            this.tPwd.PasswordChar = '\0';
+            this.tPwd.PasswordChar = '*';
             this.tPwd.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.tPwd.SelectedText = "";
-            this.tPwd.Size = new System.Drawing.Size(200, 22);
+            this.tPwd.Size = new System.Drawing.Size(210, 22);
             this.tPwd.TabIndex = 1;
             this.tPwd.UseSelectable = true;
             this.tPwd.UseStyleColors = true;
@@ -172,19 +178,126 @@
             // 
             this.tLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.tLogin.Lines = new string[0];
-            this.tLogin.Location = new System.Drawing.Point(250, 24);
+            this.tLogin.Location = new System.Drawing.Point(240, 124);
             this.tLogin.Margin = new System.Windows.Forms.Padding(0);
             this.tLogin.MaxLength = 32767;
             this.tLogin.Name = "tLogin";
             this.tLogin.PasswordChar = '\0';
             this.tLogin.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.tLogin.SelectedText = "";
-            this.tLogin.Size = new System.Drawing.Size(200, 22);
+            this.tLogin.Size = new System.Drawing.Size(210, 22);
             this.tLogin.TabIndex = 0;
             this.tLogin.UseSelectable = true;
             this.tLogin.UseStyleColors = true;
             this.tLogin.WordWrap = false;
             this.tLogin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tLogin_KeyPress);
+            // 
+            // metroProgressSpinner1
+            // 
+            this.metroProgressSpinner1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.metroProgressSpinner1.Location = new System.Drawing.Point(474, 124);
+            this.metroProgressSpinner1.Maximum = 100;
+            this.metroProgressSpinner1.Name = "metroProgressSpinner1";
+            this.tableLayoutPanel1.SetRowSpan(this.metroProgressSpinner1, 3);
+            this.metroProgressSpinner1.Size = new System.Drawing.Size(102, 86);
+            this.metroProgressSpinner1.TabIndex = 14;
+            this.metroProgressSpinner1.UseSelectable = true;
+            this.metroProgressSpinner1.UseStyleColors = true;
+            this.metroProgressSpinner1.Visible = false;
+            // 
+            // ILastLogin
+            // 
+            this.ILastLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ILastLogin.Location = new System.Drawing.Point(150, 94);
+            this.ILastLogin.Margin = new System.Windows.Forms.Padding(0);
+            this.ILastLogin.Name = "ILastLogin";
+            this.ILastLogin.Size = new System.Drawing.Size(90, 22);
+            this.ILastLogin.TabIndex = 15;
+            this.ILastLogin.Text = "Last Login:";
+            this.ILastLogin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ILastLogin.UseStyleColors = true;
+            this.ILastLogin.Visible = false;
+            // 
+            // ILastName
+            // 
+            this.ILastName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ILastName.Location = new System.Drawing.Point(150, 64);
+            this.ILastName.Margin = new System.Windows.Forms.Padding(0);
+            this.ILastName.Name = "ILastName";
+            this.ILastName.Size = new System.Drawing.Size(90, 22);
+            this.ILastName.TabIndex = 16;
+            this.ILastName.Text = "Last Name:";
+            this.ILastName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ILastName.UseStyleColors = true;
+            this.ILastName.Visible = false;
+            this.ILastName.Click += new System.EventHandler(this.metroLabel2_Click);
+            // 
+            // tlastname
+            // 
+            this.tlastname.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlastname.Lines = new string[0];
+            this.tlastname.Location = new System.Drawing.Point(240, 64);
+            this.tlastname.Margin = new System.Windows.Forms.Padding(0);
+            this.tlastname.MaxLength = 32767;
+            this.tlastname.Name = "tlastname";
+            this.tlastname.PasswordChar = '\0';
+            this.tlastname.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tlastname.SelectedText = "";
+            this.tlastname.Size = new System.Drawing.Size(210, 22);
+            this.tlastname.TabIndex = 17;
+            this.tlastname.UseSelectable = true;
+            this.tlastname.UseStyleColors = true;
+            this.tlastname.Visible = false;
+            this.tlastname.WordWrap = false;
+            // 
+            // tlastlogin
+            // 
+            this.tlastlogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlastlogin.Lines = new string[0];
+            this.tlastlogin.Location = new System.Drawing.Point(240, 94);
+            this.tlastlogin.Margin = new System.Windows.Forms.Padding(0);
+            this.tlastlogin.MaxLength = 32767;
+            this.tlastlogin.Name = "tlastlogin";
+            this.tlastlogin.PasswordChar = '\0';
+            this.tlastlogin.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tlastlogin.SelectedText = "";
+            this.tlastlogin.Size = new System.Drawing.Size(210, 22);
+            this.tlastlogin.TabIndex = 18;
+            this.tlastlogin.UseSelectable = true;
+            this.tlastlogin.UseStyleColors = true;
+            this.tlastlogin.Visible = false;
+            this.tlastlogin.WordWrap = false;
+            // 
+            // tfirstname
+            // 
+            this.tfirstname.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tfirstname.Lines = new string[0];
+            this.tfirstname.Location = new System.Drawing.Point(240, 34);
+            this.tfirstname.Margin = new System.Windows.Forms.Padding(0);
+            this.tfirstname.MaxLength = 32767;
+            this.tfirstname.Name = "tfirstname";
+            this.tfirstname.PasswordChar = '\0';
+            this.tfirstname.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tfirstname.SelectedText = "";
+            this.tfirstname.Size = new System.Drawing.Size(210, 22);
+            this.tfirstname.TabIndex = 19;
+            this.tfirstname.UseSelectable = true;
+            this.tfirstname.UseStyleColors = true;
+            this.tfirstname.Visible = false;
+            this.tfirstname.WordWrap = false;
+            // 
+            // IFirstName
+            // 
+            this.IFirstName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.IFirstName.Location = new System.Drawing.Point(150, 34);
+            this.IFirstName.Margin = new System.Windows.Forms.Padding(0);
+            this.IFirstName.Name = "IFirstName";
+            this.IFirstName.Size = new System.Drawing.Size(90, 22);
+            this.IFirstName.TabIndex = 20;
+            this.IFirstName.Text = "First Name:";
+            this.IFirstName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.IFirstName.UseStyleColors = true;
+            this.IFirstName.Visible = false;
             // 
             // LoginInput
             // 
@@ -192,8 +305,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panelbody);
             this.Name = "LoginInput";
-            this.Size = new System.Drawing.Size(600, 110);
+            this.Size = new System.Drawing.Size(600, 242);
             this.panelbody.ResumeLayout(false);
+            this.panelbody.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -203,14 +317,20 @@
         #endregion
 
         private System.Windows.Forms.Panel panelbody;
-        private MetroFramework.Controls.MetroButton bLog;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private MetroFramework.Controls.MetroButton bLog;
         private MetroFramework.Controls.MetroLabel lLogin;
         private MetroFramework.Controls.MetroLabel lPwd;
         private MetroFramework.Controls.MetroTextBox tPwd;
         private MetroFramework.Controls.MetroTextBox tLogin;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private MetroFramework.Controls.MetroProgressSpinner metroProgressSpinner1;
+        private MetroFramework.Controls.MetroLabel ILastLogin;
+        private MetroFramework.Controls.MetroLabel ILastName;
+        private MetroFramework.Controls.MetroTextBox tlastname;
+        private MetroFramework.Controls.MetroTextBox tlastlogin;
+        private MetroFramework.Controls.MetroTextBox tfirstname;
+        private MetroFramework.Controls.MetroLabel IFirstName;
 
 
     }
